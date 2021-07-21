30954071ac722ed0fefd8759b89eb1a8776aa897
Darshan <wadekar.darshan04@gmail.com>
2021-07-21 21:07:45 +0530

    upload vnetsubnet

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -11,6 +11,6 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
 resource "azurerm_subnet" "myterraformsubnet" {
   name                 = var.subnetname
   resource_group_name = azurerm_resource_group.myterraformgroup.name
-  virtual_network_name = azurerm_virtual_network..myterraformnetwork.name
+  virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
   address_prefixes     = ["10.0.2.0/24"]
 }
